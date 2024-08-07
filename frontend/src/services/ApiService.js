04ead067b538beb6a714d6303e27c3fbd2e8a86b
wilberforce001 <kenmisik@gmail.com>
2024-08-07 15:59:10 +0300

    Updated code to use the environment variable for the baseURL

--- frontend/src/services/ApiService.js
+++ frontend/src/services/ApiService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: process.env.VUE_APP_API_BASE_URL, 
   headers: {
     'Content-Type': 'application/json',
   },
